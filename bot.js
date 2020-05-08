60fdd08460ac56a3b6c7440f4bbb105923405d59
MLGGeek <65030847+MLGGeek@users.noreply.github.com>
2020-05-08 18:59:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ const querystring = require('querystring');
 const client = new Discord.Client();
 const prefix = '!';
 
-
-
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
 client.on("ready", () => {
